19c500ef3212a96f67b26fba8d0ddddf5cf5afa9
connor worthen <connorw220@protonmail.com>
2019-10-28 08:25:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,9 +17,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  find_max_value([1,2,1,3,4,3,5,4,3,2,1])
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+
 end
