9b02abf1cf996650fc59e9c6d70e16d28a3573a6
connor worthen <connorw220@protonmail.com>
2019-10-28 08:28:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,19 +1,5 @@
 def find_element_index(array, value_to_find)
-  array.find_index()
-
-  found_index = nil
-  counter = 0
-
-  while counter < array.size do
-
-    array.found_index(value_to_find)
-
-    counter += 1
-  end
-
-  found_index
-
-
+  array.index
 end
 
 def find_max_value(array)
